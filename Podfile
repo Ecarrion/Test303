0f58f2216a29a40ab2f7d261316283568ec574d4
Ernesto Carrion <ernesto@ride.com>
2016-09-08 15:19:56 -0500

    Adds Empty project + cocoapods dependencies

---
+++ Podfile
@@ -0,0 +1,16 @@
+platform :ios, '9.0'
+inhibit_all_warnings!
+
+target 'Test303' do
+  use_frameworks!
+  pod 'Alamofire', '~> 3.4'
+  pod 'RxSwift', '~> 2.6'
+  pod 'SwiftyJSON'
+end
+
+target 'Test303Tests' do
+  use_frameworks!
+  pod 'Alamofire', '~> 3.4'
+  pod 'Mockingjay'
+  pod 'RxBlocking', '~> 2.6'
+end
---
+++ Test303/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Test303
+//
+//  Created by Ernesto Carrion on 9/8/16.
+//  Copyright © 2016 ecarrion. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
