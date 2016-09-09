0a35a5027a74a9b41756aa54cee1867ad8a26828
Ernesto Carrion <ernesto@ride.com>
2016-09-09 11:04:32 -0500

    Adds skeleton for main view controller

---
+++ Test303/App/Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Test303
+//
+//  Created by Ernesto Carrion on 9/8/16.
+//  Copyright © 2016 ecarrion. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
